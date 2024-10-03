3b306759816aa1dfb3b4027fd965f67a18f4168d
Mukul Vishwakarma <mukulr@hdfclife.com>
2024-10-03 16:21:25 +0530

    added app file with mongodb configuration and installation

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Handle 404 error
+app.use(function (req, res, next) {
+  throw new Error(`Resource not found : ${req.originalUrl}`)
+})
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: false })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
